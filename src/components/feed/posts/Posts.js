b3988c2cd2b2645efe9c6050848be96ea4f5c830
MeetCoding <meet.prakash.shah@gmail.com>
2021-07-30 21:51:40 +0530

    created post component list

--- src/components/feed/posts/Posts.js
+++ src/components/feed/posts/Posts.js
@@ -1,8 +1,23 @@
 import React, { Component } from 'react';
-import './Posts.css';
+import Post from './post/Post';
 
 export class Posts extends Component {
     render() {
+
+        let currentKey = 0;
+
+        const posts = this.props.data.map(post => {
+            currentKey++;
+            return (
+                <Post 
+                    key={currentKey}
+                    name={post.name}
+                    photo={post.photo}
+                    text={post.text}
+                />
+            )
+        })
+
         return (
             <div>
                 
