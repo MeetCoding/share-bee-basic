0890997bc9e041edcdc42105c864fb4783478216
MeetCoding <meet.prakash.shah@gmail.com>
2021-07-31 07:46:14 +0530

    created the like button

--- src/components/feed/posts/post/Post.js
+++ src/components/feed/posts/post/Post.js
@@ -1,8 +1,28 @@
 import React, { Component } from 'react';
-import './Post.css'
+import './Post.css';
+import FavoriteBorderIcon from '@material-ui/icons/FavoriteBorder';
+import FavoriteIcon from '@material-ui/icons/Favorite';
 
 export class Post extends Component {
+
+    constructor() {
+        super();
+        this.state = {
+            isLiked: false
+        }
+        this.handleLikeClick = this.handleLikeClick.bind(this);
+    }
+
+    handleLikeClick() {
+        this.setState(prevState => {
+            return {
+                isLiked: !prevState.isLiked
+            }    
+        });
+    }
+
     render() {
+        
         return (
             <div className='post'>
                 <img 
@@ -14,6 +34,17 @@ export class Post extends Component {
                     <h1 className='post-name'>{this.props.name}</h1>
                     <p className='post-text'>{this.props.text}</p>
                 </div>
+                {
+                    this.state.isLiked? 
+                    <FavoriteIcon 
+                        className='like-btn filled-like-btn' 
+                        onClick={this.handleLikeClick} 
+                    /> : 
+                    <FavoriteBorderIcon 
+                        className='like-btn' 
+                        onClick={this.handleLikeClick} 
+                    />
+                }
             </div>
         )
     }
